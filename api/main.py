9d92fbf261d286c956431c80144fb6b0f2f9979f
akshatcx <akshat.c2k@gmail.com>
2022-05-15 17:37:42 +0530

    add black formatting

--- api/main.py
+++ api/main.py
@@ -24,14 +24,17 @@ async def startup_event():
 def ping():
     return {"ping": "pong"}
 
+
 @app.post("/accounts/", response_model=schemas.Account)
 def create_account(account: schemas.AccountIn, db: Session = Depends(get_db)):
     return crud.create_account(db=db, account=account)
+
 
 @app.get("/accounts/", response_model=List[schemas.Account])
 def read_accounts(db: Session = Depends(get_db)):
     accounts = crud.get_accounts(db)
     return accounts
+
 
 @app.get("/accounts/{account_id}", response_model=schemas.Account)
 def read_account(account_id: int, db: Session = Depends(get_db)):
@@ -41,20 +44,25 @@ def read_account(account_id: int, db: Session = Depends(get_db)):
     return db_account
 
 
-@app.get("/accounts/{account_id}/transactions/", response_model=List[schemas.Transaction])
-def get_transactions_by_account(
-    account_id: int, db: Session = Depends(get_db)
-):
+@app.get(
+    "/accounts/{account_id}/transactions/", response_model=List[schemas.Transaction]
+)
+def get_transactions_by_account(account_id: int, db: Session = Depends(get_db)):
     return crud.get_transactions_by_acc(db=db, id=account_id)
 
+
 @app.post("/transactions/", response_model=schemas.Transaction)
-def create_transaction(transaction: schemas.TransactionIn, db: Session = Depends(get_db)):
+def create_transaction(
+    transaction: schemas.TransactionIn, db: Session = Depends(get_db)
+):
     return crud.create_transaction(db=db, transaction=transaction)
+
 
 @app.get("/transactions/", response_model=List[schemas.Transaction])
 def read_transactions(db: Session = Depends(get_db)):
     transactions = crud.get_transactions(db)
     return transactions
+
 
 # run server
 if __name__ == "__main__":
--- api/server/crud.py
+++ api/server/crud.py
@@ -2,20 +2,26 @@ from sqlalchemy.orm import Session
 
 from server import models, schemas
 
+
 def get_account(db: Session, id: int):
     return db.query(models.Account).filter(models.Account.id == id).first()
+
 
 def get_accounts(db: Session):
     return db.query(models.Account).all()
 
+
 def get_transaction(db: Session, id: int):
     return db.query(models.Transaction).filter(models.Transaction.id == id).first()
+
 
 def get_transactions_by_acc(db: Session, id: int):
     return db.query(models.Transaction).filter(models.Transaction.acc_id == id).all()
 
+
 def get_transactions(db: Session):
     return db.query(models.Transaction).all()
+
 
 def create_account(db: Session, account: schemas.AccountIn):
     db_account = models.Account(**account.dict())
@@ -23,6 +29,7 @@ def create_account(db: Session, account: schemas.AccountIn):
     db.commit()
     db.refresh(db_account)
     return db_account
+
 
 def create_transaction(db: Session, transaction: schemas.TransactionIn):
     db_transaction = models.Transaction(**transaction.dict())
